1b614abdd558060f76bfd4b20c1764bb17073d34
Yanis Kara <yanis.kara@edu.esiee.fr>
2024-01-01 20:12:21 +0100

    initial commit

---
+++ RK_PriseEnMain_Moteurs.s
@@ -0,0 +1,251 @@
+	;; RK - Evalbot (Cortex M3 de Texas Instrument)
+; programme - Pilotage 2 Moteurs Evalbot par PWM tout en ASM (Evalbot tourne sur lui même)
+		
+		
+		
+		AREA    |.text|, CODE, READONLY
+		
+; This register controls the clock gating logic in normal Run mode
+SYSCTL_PERIPH_GPIO EQU		0x400FE108	; SYSCTL_RCGC2_R (p291 datasheet de lm3s9b92.pdf)
+
+; The GPIODATA register is the data register
+GPIO_PORTF_BASE		EQU		0x40025000	; GPIO Port F (APB) base: 0x4002.5000 (p416 datasheet de lm3s9B92.pdf)
+
+; The GPIODATA register is the data register
+GPIO_PORTD_BASE		EQU		0x40007000		; GPIO Port D (APB) base: 0x4000.7000 (p416 datasheet de lm3s9B92.pdf)
+
+; The GPIODATA register is the data register
+GPIO_PORTE_BASE		EQU		0x40024000		; GPIO Port E (APB) base: 0x4002.4000 (p416 datasheet de lm3s9B92.pdf)
+
+; configure the corresponding pin to be an output
+; all GPIO pins are inputs by default
+GPIO_O_DIR   		EQU 	0x00000400  ; GPIO Direction (p417 datasheet de lm3s9B92.pdf)
+
+; The GPIODR2R register is the 2-mA drive control register
+; By default, all GPIO pins have 2-mA drive.
+GPIO_O_DR2R   		EQU 	0x00000500  ; GPIO 2-mA Drive Select (p428 datasheet de lm3s9B92.pdf)
+
+; Digital enable register
+; To use the pin as a digital input or output, the corresponding GPIODEN bit must be set.
+GPIO_O_DEN  		EQU 	0x0000051C  ; GPIO Digital Enable (p437 datasheet de lm3s9B92.pdf)
+
+; Pul_up
+GPIO_I_PUR   		EQU 	0x00000510  ; GPIO Pull-Up (p432 datasheet de lm3s9B92.pdf)
+
+; Broches select
+BROCHE4_5			EQU		0x30		; led1 & led2 sur broche 4 et 5
+
+BROCHE6				EQU 	0x40		; bouton poussoir 1
+	
+BROCHE7				EQU		0x80		; bouton poussoir 2
+
+BROCHE6_7			EQU 	0xC0		; bouton poussoit 1 et 2
+	
+BROCHE0				EQU		0x01		;bumper 1
+
+BROCHE1				EQU 	0x02		;bumper 2
+	
+BROCHE0_1			EQU  	0x03		;bumper 1 et 2
+	
+Led_Off				EQU		0x00		;pour eteindre une led 
+	
+; blinking frequency
+DUREE   			EQU     0x0005FFFF
+	
+; turn frequency
+DUREE_TURN   		EQU     0x015FFFFF
+			
+			
+		ENTRY
+		EXPORT	__main    
+		
+		;; The IMPORT command specifies that a symbol is defined in a shared object at runtime.
+		IMPORT	MOTEUR_INIT					; initialise les moteurs (configure les pwms + GPIO)
+		
+		IMPORT	MOTEUR_DROIT_ON				; activer le moteur droit
+		IMPORT  MOTEUR_DROIT_OFF			; déactiver le moteur droit
+		IMPORT  MOTEUR_DROIT_AVANT			; moteur droit tourne vers l'avant
+		IMPORT  MOTEUR_DROIT_ARRIERE		; moteur droit tourne vers l'arrière
+		IMPORT  MOTEUR_DROIT_INVERSE		; inverse le sens de rotation du moteur droit
+		
+		IMPORT	MOTEUR_GAUCHE_ON			; activer le moteur gauche
+		IMPORT  MOTEUR_GAUCHE_OFF			; déactiver le moteur gauche
+		IMPORT  MOTEUR_GAUCHE_AVANT			; moteur gauche tourne vers l'avant
+		IMPORT  MOTEUR_GAUCHE_ARRIERE		; moteur gauche tourne vers l'arrière
+		IMPORT  MOTEUR_GAUCHE_INVERSE		; inverse le sens de rotation du moteur gauche
+
+
+__main	
+
+		; ;; Enable the Port F & D peripheral clock 		(p291 datasheet de lm3s9B96.pdf)
+		; ;;									
+		ldr r6, = SYSCTL_PERIPH_GPIO  			;; RCGC2
+        mov r0, #0x00000038  					;; Enable clock sur GPIO D et F où sont branchés les leds (0x28 == 0b101000)
+		; ;;														 									      (GPIO::FEDCBA)
+        str r0, [r6]
+		
+		; ;; "There must be a delay of 3 system clocks before any GPIO reg. access  (p413 datasheet de lm3s9B92.pdf)
+		nop	   									;; tres tres important....
+		nop	   
+		nop	   									;; pas necessaire en simu ou en debbug step by step...
+	
+	
+		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION LED
+
+        ldr r6, = GPIO_PORTF_BASE+GPIO_O_DIR    ;; 1 Pin du portF en sortie (broche 4 : 00010000)
+        ldr r0, = BROCHE4_5 	
+        str r0, [r6]
+		
+		ldr r6, = GPIO_PORTF_BASE+GPIO_O_DEN	;; Enable Digital Function 
+        ldr r0, = BROCHE4_5		
+        str r0, [r6]
+		
+		ldr r6, = GPIO_PORTF_BASE+GPIO_O_DR2R	;; Choix de l'intensité de sortie (2mA)
+        ldr r0, = BROCHE4_5			
+        str r0, [r6]
+		
+		mov r2, #0x000       					;; pour eteindre LED
+     
+		; allumer la led broche 4 (BROCHE4_5)
+		mov r3, #BROCHE4_5		;; Allume LED1&2 portF broche 4&5 : 00110000
+		
+		ldr r8, = GPIO_PORTF_BASE + (BROCHE4_5<<2)  ;; @data Register = @base + (mask<<2) ==> LED1
+		;vvvvvvvvvvvvvvvvvvvvvvvFin configuration LED 
+
+
+		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION switch 1 & 2
+
+		ldr r6, = GPIO_PORTD_BASE+GPIO_I_PUR	;; Pul_up 
+        ldr r0, = BROCHE6_7	
+        str r0, [r6]
+		
+		ldr r6, = GPIO_PORTD_BASE+GPIO_O_DEN	;; Enable Digital Function 
+        ldr r0, = BROCHE6_7	
+        str r0, [r6]     
+		
+		ldr r7, = GPIO_PORTD_BASE + (BROCHE6_7<<2)  ;; @data Register = @base + (mask<<2) ==> Switcher
+		
+		
+		;vvvvvvvvvvvvvvvvvvvvvvvFin configuration Bumper 1 & 2
+		
+		ldr r6, = GPIO_PORTE_BASE+GPIO_I_PUR	;; Pul_up 
+        ldr r0, = BROCHE0_1	
+        str r0, [r6]
+		
+		ldr r6, = GPIO_PORTE_BASE+GPIO_O_DEN	;; Enable Digital Function 
+        ldr r0, = BROCHE0_1	
+        str r0, [r6]     
+		
+		ldr r9, = GPIO_PORTE_BASE + (BROCHE0_1<<2)  ;; @data Register = @base + (mask<<2) ==> Bumper
+		;vvvvvvvvvvvvvvvvvvvvvvvFin configuration Switcher 
+		
+		BL	MOTEUR_INIT	
+
+Moteur_Off
+		BL MOTEUR_DROIT_OFF
+		BL MOTEUR_GAUCHE_OFF
+		
+ReadState_Launch
+		ldr r10,[r7]
+		CMP r10,#0x80
+		str r2, [r8]
+		BNE ReadState_Launch      
+		
+		;r0 r1 r6 used
+		
+		str r3,[r8] 
+
+Mode_Direction
+		ldr r4,[r9]
+		CMP r4,#0x02
+		BEQ	Mode_Right
+
+		ldr r4,[r9]
+		CMP r4,#0x01
+		BEQ	Mode_Left
+		
+		BL 	Mode_Direction
+	
+Mode_Right
+		mov r12, #0x01
+		ldr r5, = 0x000FFFFF
+		str r2,[r8] 
+		BL 	timer
+
+Mode_Left
+		mov r12, #0x02
+		ldr r5, = 0x000FFFFF
+		str r2,[r8] 
+timer
+		
+		subs r5, #1
+        bne timer
+		
+loop	
+		; Activer les deux moteurs droit et gauche
+		BL	MOTEUR_DROIT_ON
+		BL	MOTEUR_GAUCHE_ON
+		
+		; Evalbot avance droit devant
+		BL	MOTEUR_DROIT_AVANT	   
+		BL	MOTEUR_GAUCHE_AVANT
+		
+		BL	BumperState
+		BL	ReadState_Stop
+		
+		str r2, [r8] ;eteindre led1 et led2
+		ldr r11, = DUREE 		
+		
+wait	
+		BL	BumperState
+		subs r11, #1
+		BNE wait
+		
+		str r3, [r8]  							;; Allume LED1&2 portF broche 4&5 : 00110000 (contenu de r3)
+        ldr r11, = DUREE						;; pour la duree de la boucle d'attente (wait1)
+wait1	
+		BL	BumperState
+		subs r11, #1
+		BNE wait1
+		
+		b	loop
+
+BumperState
+		;compare with left bumper 1
+		ldr r4,[r9]
+		CMP r4,#0x01
+		BEQ Turn
+		CMP r4, #0x02
+		BEQ Turn
+		BX	LR
+		
+Turn
+		CMP r12,#0x01
+		BEQ	Turn_Right
+		BL	Turn_Left
+		
+Turn_Right
+		BL 	MOTEUR_GAUCHE_ARRIERE
+		ldr r5, = DUREE_TURN
+		str r3, [r8] 
+		BL	wait_turn
+	
+Turn_Left
+		BL 	MOTEUR_DROIT_ARRIERE
+		ldr r5, = DUREE_TURN
+		str r3, [r8] 
+		BL	wait_turn
+
+wait_turn
+		subs r5, #1
+		BNE wait_turn
+		b loop
+
+ReadState_Stop
+		ldr r10,[r7]
+		CMP r10,#0x40
+		BEQ	Moteur_Off
+		BX	LR 
+		
+		NOP
+        END
