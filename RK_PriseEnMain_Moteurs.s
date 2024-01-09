39f353a7bd5b4d14aea7e8125da3e84d6771a9f0
Yanis Kara <yanis.kara@edu.esiee.fr>
2024-01-09 13:10:33 +0100

    p

--- RK_PriseEnMain_Moteurs.s
+++ RK_PriseEnMain_Moteurs.s
@@ -54,21 +54,15 @@ BROCHE0_1			EQU  	0x03		;bumper 1 et 2
 Led_Off				EQU		0x00		;pour eteindre une led 
 	
 DUREE   			EQU     0x0005FFFF	; blinking frequency
-
-DUREE_TURN   		EQU     0x015FFFFF	; turn frequency
-
-DUREE   			EQU     0x0005FFFF	; blinking frequency
 	
 DUREE_TURN   		EQU     0x015FFFFF	; turn frequency
 
 DUREE_DODGE			EQU		0x015FFFFF	; Dodge frequency
 			
 DUREE_DISPLAY		EQU		0x004FFFFF	; Show frequency
-	
-DUREE_BACK			EQU		0x003FFFFF	; Back frequency
 
 DUREE_BACK			EQU		0x005FFFFF	; Back frequency
-	
+
 		ENTRY
 		EXPORT	__main    
 		
